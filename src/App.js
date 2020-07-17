04c9b56c54975e2cb5bae0bbd71d039c75dd872a
Syedarqum <sp19bsse0058@maju.edu.pk>
2020-07-17 22:12:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1> hello bhai loog</h1>
-      
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js 12345 </code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
